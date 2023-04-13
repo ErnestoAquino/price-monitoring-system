a5a7d100aac73853e21eedd7d6db1aa19c259a89
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2023-04-13 14:26:27 +0200

    The method that creates the file has been changed to receive the desired name as a parameter.

--- download_information_books.py
+++ download_information_books.py
@@ -4,14 +4,26 @@ from bs4 import BeautifulSoup
 from get_information_book import get_information_book
 
 
-def download_information_book_of(category):
-    write_header(category)
+def download_information():
+    url_page = "https://books.toscrape.com/index.html"
+    response = requests.get(url_page)
+    dictionary_of_category = {}
+    if response.status_code:
+        soup = BeautifulSoup(response.content, "html.parser")
+        # print(soup.find("ul", class_ = "nav-list").findAll("a", href = True))
+        for link in soup.find("ul", class_ = "nav-list").find("ul").findAll("a", href = True):
+            dictionary_of_category[link.string.strip()] = link["href"]
+            # print(link.text, " - ", link["href"])
+
+
+def download_information_book_of(file_name, category):
+    write_header(file_name)
     pages_of_category = find_all_pages_of(category)
     links_of_books = find_all_links_books_category(pages_of_category)
-    get_books_from_links(links_of_books, category)
+    get_books_from_links(links_of_books, file_name)
 
 
-def write_header(category):
+def write_header(file_name):
     header = [
         "product_page_url",
         "universal_ product_code (upc)",
@@ -24,14 +36,16 @@ def write_header(category):
         "review_rating",
         "image_url"
     ]
-    with open(category + ".csv", "w") as file:
+    with open(file_name + ".csv", "w") as file:
         write = csv.writer(file, delimiter = ",")
         write.writerow(header)
 
 
 def find_all_pages_of(category):
     index_page = "/index.html"
-    url_complete = "https://books.toscrape.com/catalogue/category/books/" + category + index_page
+    category_without_index = category.removesuffix("/index.html")
+    # url_complete = "https://books.toscrape.com/catalogue/category/books/" + category + index_page
+    url_complete = "https://books.toscrape.com/" + category_without_index + index_page
     are_more_books = True
     urls = [url_complete]
     while are_more_books:
@@ -40,14 +54,14 @@ def find_all_pages_of(category):
             soup = BeautifulSoup(response.content, "html.parser")
             if soup.find("li", class_ = "next"):
                 index_page = "/" + soup.find("li", class_ = "next").find("a")["href"]
-                url_complete = "https://books.toscrape.com/catalogue/category/books/" + category + index_page
+                # url_complete = "https://books.toscrape.com/catalogue/category/books/" + category + index_page
+                url_complete = "https://books.toscrape.com/" + category_without_index + index_page
                 urls.append(url_complete)
             else:
                 are_more_books = False
         else:
             are_more_books = False
     return urls
-    # find_all_links_books_category(urls, category)
 
 
 def find_all_links_books_category(urls_of_category):
@@ -72,12 +86,12 @@ def create_absolutes_urls(links):
     return complete_links
 
 
-def get_books_from_links(links, category):
+def get_books_from_links(links, file_name):
     for link in links:
-        add_book_information(get_information_book(link), category)
+        add_book_information(get_information_book(link), file_name)
 
 
-def add_book_information(information_book, category):
-    with open(category + ".csv", "a") as file_csv:
+def add_book_information(information_book, file_name):
+    with open(file_name + ".csv", "a") as file_csv:
         writer = csv.writer(file_csv, delimiter = ",")
         writer.writerow(information_book)
